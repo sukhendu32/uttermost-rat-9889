8458506903b9b2fba97ca26fec7177141ae99b1e
SukhenduPaul <sukhendupaul36@gmail.com>
2022-09-28 22:17:53 +0530

    connection

--- OnlineBankingSystem/src/com/bank/utility/DButil.java
+++ OnlineBankingSystem/src/com/bank/utility/DButil.java
@@ -1,5 +1,38 @@
 package com.bank.utility;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DButil {
+	
+	
+public static Connection provideConnection() {
+		
+		Connection conn=null;
+		
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String url="jdbc:mysql://localhost:3306/OBS";
+		
+		try {
+			conn= DriverManager.getConnection(url,"root","8797");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		return conn;
+		
+	}
 
 }
